b69aaf89fffb5dd9ad5379ab54ad0b84633757ab
Huilin Qu <huilin.qu@cern.ch>
2022-07-22 10:56:28 +0800

    Update dataset URLs.

--- get_datasets.py
+++ get_datasets.py
@@ -9,20 +9,20 @@ from utils.dataset_utils import get_file, extract_archive
 datasets = {
     'JetClass': {
         'Pythia/train_100M': [
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_train_100M_part0.tar', 'de4fd2dca2e68ab3c85d5cfd3bcc65c3'),
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_train_100M_part1.tar', '9722a359c5ef697bea0fbf79bf50f003'),
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_train_100M_part2.tar', '1e9f66cd1f915f9d10e90ae1d7761720'),
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_train_100M_part3.tar', '47348fc8985319fa4806da87500482fa'),
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_train_100M_part4.tar', '6b0ce16bd93b442a8d51914466990279'),
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_train_100M_part5.tar', '416e347512e716de51d392bee327b8e9'),
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_train_100M_part6.tar', 'e9b9c1557b1b39bf0a16e4ab631ae451'),
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_train_100M_part7.tar', '5bfc6cb285ccb7680cefa9ac82ad1a2e'),
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_train_100M_part8.tar', '540c1a0d66dfad78d2b363c5740ccf86'),
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_train_100M_part9.tar', '668f40b3275167ff7104c48317c0ae2a'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_train_100M_part0.tar', 'de4fd2dca2e68ab3c85d5cfd3bcc65c3'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_train_100M_part1.tar', '9722a359c5ef697bea0fbf79bf50f003'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_train_100M_part2.tar', '1e9f66cd1f915f9d10e90ae1d7761720'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_train_100M_part3.tar', '47348fc8985319fa4806da87500482fa'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_train_100M_part4.tar', '6b0ce16bd93b442a8d51914466990279'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_train_100M_part5.tar', '416e347512e716de51d392bee327b8e9'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_train_100M_part6.tar', 'e9b9c1557b1b39bf0a16e4ab631ae451'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_train_100M_part7.tar', '5bfc6cb285ccb7680cefa9ac82ad1a2e'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_train_100M_part8.tar', '540c1a0d66dfad78d2b363c5740ccf86'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_train_100M_part9.tar', '668f40b3275167ff7104c48317c0ae2a'),
         ],
         'Pythia/': [
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_val_5M.tar', '7235ccb577ed85023ea3ab4d5e6160cf'),
-            ('https://hqu.web.cern.ch/datasets/JetClass/JetClass_Pythia_test_20M.tar', '64e5156d26d101adeb43b8388207d767'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_val_5M.tar', '7235ccb577ed85023ea3ab4d5e6160cf'),
+            ('https://zenodo.org/record/6619768/files/JetClass_Pythia_test_20M.tar', '64e5156d26d101adeb43b8388207d767'),
         ],
     },
     'TopLandscape': {
